a2b84124f348e8c5d4a64c2e247b948d49cc2a7b
Rajat Bhatt <rajatbhatt999@gmail.com>
2021-08-24 19:05:13 +0530

    Add stipe backend for payment

--- client/src/pages/checkout/checkout.js
+++ client/src/pages/checkout/checkout.js
@@ -1,10 +1,33 @@
 import { useContext } from "react";
 import CheckoutItem from "../../components/checkout-item/CheckoutItem";
 import { AuthContext } from "../../context/auth";
-import StripeCard from "../../stripe/Card-Minimal";
+import { loadStripe } from "@stripe/stripe-js";
 import "./checkout.scss";
 const Checkout = () => {
   const authctx = useContext(AuthContext);
+  const stripePromise = loadStripe(
+    "pk_test_51JMwqTSGZ43n5aWdTPNfq7Y8I34INVfxGyw0hLsJDHtAG6DytKfKECQvulVCI4W1Ipb4gKzu3g6FmUOci1SItfaW00zcN42hcz"
+  );
+
+  const createCheckoutSession = async () => {
+    console.log("working");
+    const stripe = await stripePromise;
+    const res = await fetch("/create-checkout-session", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ items: authctx.cartStore.items }),
+    });
+    const checkoutSession = await res.json();
+    const result = stripe.redirectToCheckout({
+      sessionId: checkoutSession.id,
+    });
+
+    if (result.error) {
+      alert(result.error.message);
+    }
+  };
   return (
     <section className="checkout-page">
       <header className="checkout-header">
@@ -28,7 +51,10 @@ const Checkout = () => {
         <CheckoutItem key={cartItem.id} item={cartItem} />
       ))}
       <div className="total">Total: $ {authctx.cartStore.totalPrice}</div>
-      <StripeCard />
+
+      <button role="link" onClick={createCheckoutSession}>
+        Checkout
+      </button>
     </section>
   );
 };
---
+++ server.js
@@ -0,0 +1,56 @@
+const express = require("express");
+const app = express();
+if (process.env.NODE_ENV !== "production") require("dotenv").config();
+
+// This is your real test secret API key.
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+const port = process.env.PORT || 5000;
+
+app.use(express.static("."));
+app.use(express.json());
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "client/build")));
+
+  app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "client/build", "index.html"));
+  });
+}
+
+const calculateOrderAmount = (items) => {
+  // Replace this constant with a calculation of the order's amount
+  // Calculate the order total on the server to prevent
+  // people from directly manipulating the amount on the client
+  return 1400;
+};
+
+app.post("/create-checkout-session", async (req, res) => {
+  const { items } = req.body;
+
+  const transformedItems = items.map((item) => ({
+    quantity: item.quantity,
+    price_data: {
+      currency: "inr",
+      unit_amount: item.price * 100,
+      product_data: {
+        name: item.name,
+        images: [item.imageUrl],
+      },
+    },
+  }));
+  console.log(transformedItems);
+  const session = await stripe.checkout.sessions.create({
+    payment_method_types: ["card"],
+    shipping_address_collection: {
+      allowed_countries: ["IN"],
+    },
+    shipping_rates: ["shr_1JQCAFSGZ43n5aWdIkUBxJi2"],
+    line_items: transformedItems,
+    mode: "payment",
+    success_url: `http://localhost:3000?success=true`,
+    cancel_url: `http://localhost:3000?canceled=true`,
+  });
+  res.status(200).json({ id: session.id });
+});
+
+app.listen(4242, () => console.log("Node server listening on port 4242!"));
